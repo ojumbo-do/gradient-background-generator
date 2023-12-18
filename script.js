8339264e6adf69c8f6db01939013151a3dafcff8
Dancun Ojumbo <70913596+klaus990@users.noreply.github.com>
2023-12-18 17:26:17 +0300

    Minor bug fixed

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let css = document.querySelector("h3");
 let color01 = document.querySelector(".color01");
 let color02 = document.querySelector(".color02");
-let body = document.getElementById("#whole-body");
+let body = document.getElementById("whole-body");
 
 const setGradient = function () {
-  body.style.background = linear-gradient(to right, color01.value, color02.value);
+  body.style.background = `linear-gradient(to right, ${color01.value}, ${color02.value})`;
   console.log(color01);
   console.log(color02);
   css.textContent = `${body.style.background};`;
